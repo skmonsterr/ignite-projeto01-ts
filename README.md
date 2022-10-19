af2699506a5011e5dbc63cb1ee395fe2680839e4
skmonsterr <ctjoaomartins@gmail.com>
2022-10-19 07:35:55 -0300

    Translate project JS to TS

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto 01 - Ignite Feed TypeScript
+
+<p align="center">
+  <img height="600" src="https://i.imgur.com/9BahHVG.png" />
+</p>
---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { Header } from "./components/Header/Header";
+import { Post } from "./components/Post/Post";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import styles from "./App.module.css";
+import "./global.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/48605430?v=4",
+      name: "João Martins",
+      role: "Campaign @Pmweb",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉 jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-10-05 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/48607584?v=4",
+      name: "Martins João",
+      role: "CEO @sKmEnterprise",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉 jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-10-16 20:00:00"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
